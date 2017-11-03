c1e3abdc862c314e429e77e770a437b0b7ab418b
vinayak <vinayakdeepu@gmail.com>
2017-11-04 00:18:51 +0530

    Add flasgger

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
 # from sqlalchemy import create_engine, MetaData, Table
+from flasgger import Swagger
+from flasgger import swag_from
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "PlacementAPI",
+    "description": "API for Placement App",
+    "contact": {
+      "responsibleOrganization": "Gallants",
+    },
+    "version": "0.0.1"
+  },
+  "host": "localhost:5000",  # overrides localhost:5000
+  "basePath": "/api",  # base bash for blueprint registration
+  "schemes": [
+    "http",
+    "https"
+  ],
+  "operationId": "getmyData"
+}
+swagger = Swagger(app, template=template)
 
 db = SQLAlchemy(app)
 db.init_app(app)
@@ -15,9 +37,10 @@ def hello():
     return str(db.engine.execute('select * from department').first())
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/login', methods=['POST'])
+@swag_from('docs/login.yml')
+def login():
+    return jsonify('YO')
 
 
 if __name__ == '__main__':
