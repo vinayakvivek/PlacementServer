610f6a98b973af3d28128d11025d3b3f94aaa974
vinayak <vinayakdeepu@gmail.com>
2017-11-04 20:39:46 +0530

    Add company_add_jaf API endpoint

--- app.py
+++ app.py
@@ -319,5 +319,59 @@ def resume_status():
     })
 
 
+@app.route('/company/addjaf', methods=['POST'])
+@swag_from('docs/company_add_jaf.yml')
+def add_jaf():
+    data = ""
+    status = ""
+    if 'username' not in session:
+        # no user has logged in
+        status = "false"
+        data = "Invalid Session"
+    elif session['user_type'] != 2:
+        # logged in user is not a company
+        status = "false"
+        data = "User is not a student"
+    else:
+        company_id = session['username']
+        request_data = request.get_json()
+
+        jaf_no = int(request_data['jaf_no'])
+        name = request_data['name']
+        description = request_data['description']
+        stipend = request_data['stipend']
+        cpi_cutoff = request_data['cpi_cutoff']
+
+        # check if jaf_no already exists
+        try:
+            query = """
+                select count(*)
+                from jaf
+                where company_id = %s
+                    and jaf_no = %s
+                """
+            res = list(conn.execute(query, (company_id, jaf_no, )).first())
+            if (res[0] == 1):
+                status = "false"
+                data = "JAF already exists"
+            else:
+                query = """
+                    insert into
+                    jaf(company_id, jaf_no, name, description, stipend, cpi_cutoff)
+                    values(%s, %s, %s, %s, %s, %s)
+                    """
+                conn.execute(query, (company_id, jaf_no, name, description, stipend, cpi_cutoff, ))
+                status = "true"
+                data = "successfully added new JAF"
+        except:
+            status = "false"
+            data = "database error"
+
+    return jsonify({
+        'data': data,
+        'status': status
+    })
+
+
 if __name__ == '__main__':
     app.run(threaded=True, host=URL, port=int(PORT))
