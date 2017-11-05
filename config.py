747d3f3e835832af3094df2ad3de14b2d516d997
vinayak <vinayakdeepu@gmail.com>
2017-11-05 12:33:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
 import os
 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-URL = 'localhost'
-PORT = '5000'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+URL = '<HOST_URL>'
+PORT = <HOST_PORT>
 POSTGRES = {
-    'user': 'vinayakvivek',
-    'pw': '',
-    'db': 'db_project',
-    'host': 'localhost',
-    'port': '5432',
+    'user': '<postgres_username>',
+    'pw': '<postgres_password>',
+    'db': '<database_name>',
+    'host': '<database_host_url>',
+    'port': '<database_port>',
 }
 
 DEBUG = True
-SECRET_KEY = "RGEUWGREWY"
+SECRET_KEY = "<some_secret_key>"
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
