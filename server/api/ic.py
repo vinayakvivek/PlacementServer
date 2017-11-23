b404948f2c5a9a08a531640413a3e0a940e4ac62
vinayak <vinayakdeepu@gmail.com>
2017-11-23 11:00:17 +0530

    Add IC functions (some bug fixes)

--- server/api/ic.py
+++ server/api/ic.py
@@ -61,15 +61,16 @@ def ic_view_all_jafs():
         try:
             query = """
                 select
-                    company_id,
+                    company.id,
                     jaf_no,
                     jaf.name,
                     description,
                     stipend,
                     cpi_cutoff,
-                    company.name
-                from jaf natural join eligibility
-                    join company on company_id = company.id
+                    company.name,
+                    is_verified
+                from jaf, company
+                where jaf.company_id = company.id
                 """
             res = conn.execute(query)
             data = []
@@ -96,6 +97,81 @@ def ic_view_all_jafs():
                         'description': row[3],
                         'stipend': row[4],
                         'cpi_cutoff': float(row[5]),
+                        'is_verified': row[7],
+                        'eligible_departments': eligible_departments
+                    })
+
+            status = "true"
+        except Exception as e:
+            status = "false"
+            data = str(e)
+
+    return jsonify({
+        'data': data,
+        'status': status
+    })
+
+
+@ic_blueprint.route('/ic/jafs/alloted', methods=['GET'])
+@swag_from('docs/ic_view_alloted_jafs.yml')
+def ic_view_alloted_jafs():
+    data = ""
+    status = ""
+    if 'username' not in session:
+        # no user has logged in
+        status = "false"
+        data = "Invalid Session"
+    elif session['user_type'] != 1:
+        # logged in user is not an IC
+        status = "false"
+        data = "User is not an IC"
+    else:
+        ic_id = session['username']
+
+        try:
+            query = """
+                with jafs as
+                    (select *
+                     from jaf
+                     where alloted_ic_id = %s)
+                select
+                    company.id,
+                    jaf_no,
+                    jafs.name,
+                    description,
+                    stipend,
+                    cpi_cutoff,
+                    company.name,
+                    is_verified
+                from jafs, company
+                where jafs.company_id = company.id
+                """
+            res = conn.execute(query, (ic_id))
+            data = []
+            for row in res:
+                eligible_departments = []
+                sub_query = """
+                    select id, name
+                    from eligibility join department on dept_id = id
+                    where company_id = %s
+                        and jaf_no = %s
+                    """
+                sub_res = conn.execute(sub_query, (int(row[0]), int(row[1])))
+                for dept in sub_res:
+                    eligible_departments.append({
+                            'dept_id': int(dept[0]),
+                            'name': dept[1]
+                        })
+
+                data.append({
+                        'company_id': row[0],
+                        'company_name': row[6],
+                        'jaf_no': row[1],
+                        'jaf_name': row[2],
+                        'description': row[3],
+                        'stipend': row[4],
+                        'cpi_cutoff': float(row[5]),
+                        'is_verified': row[7],
                         'eligible_departments': eligible_departments
                     })
 
